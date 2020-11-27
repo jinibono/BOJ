a52315de84ceb264291502d7497b4678635a3017
jinibono <jinibonoo@hanmail.net>
2020-11-28 00:45:41 +0900

    2020-11-28 BinaryMatch

--- src/BinaryMatch/boj_2188_축사배정.java
+++ src/BinaryMatch/boj_2188_축사배정.java
@@ -3,39 +3,39 @@ package BinaryMatch;
 import java.util.*;
 import java.io.*;
 
-public class boj_1298_노트북의주인을찾아서 {
+public class boj_2188_축사배정 {
 	static int result = 0;
 	static boolean[] visited;
-	static int[] parent; // 노트북이 선택된 사용자의 번호
-
+	static int[] parent; // 선택된 사용자의 번호
 	static ArrayList list[];
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
+
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
-		list = new ArrayList[N + 1];
-
-		parent = new int[N + 1];
-		visited = new boolean[N + 1]; // 어떤 점에서 다른 점을 방문한 적이 있는가?
+		list = new ArrayList[N+1];
+		parent = new int[M+1];
+		visited = new boolean[M+1]; // 어떤 점에서 다른 점을 방문한 적이 있는가?
 		for (int i = 1; i <= N; i++)
 			list[i] = new ArrayList<Integer>();
-
-		for (int i = 0; i < M; i++) {
+		for (int i = 1; i <= N; i++) {
 			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			list[a].add(b);
+			int S = Integer.parseInt(st.nextToken());
+			for (int c = 0; c < S; c++) {
+				int temp = Integer.parseInt(st.nextToken());
+				list[i].add(temp);
+			}
+
 		}
 		for (int i = 1; i <= N; i++) {
-			visited = new boolean[N + 1]; // 어떤 점에서 다른 점을 방문한 적이 있는가?
+			Arrays.fill(visited, false);
 			if (dfs(i))
 				result++;
 		}
-
 		System.out.println(result);
-//		System.out.println(Arrays.toString(parent));
+
 	}
 
 	static boolean dfs(int num) {
