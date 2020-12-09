5598f8940a791dc1964ccaf62a346fb855ffc64b
jinibono <jinibonoo@hanmail.net>
2020-12-09 23:50:20 +0900

    2020-12-09 SW TEST

--- src/SAMSUNG/boj_20056_마법사상어와파이어볼.java
+++ src/SAMSUNG/boj_20056_마법사상어와파이어볼.java
@@ -27,8 +27,25 @@ public class boj_20056_마법사상어와파이어볼 {
 		}
 
 		public void move() {
-			x = this.x + dr[d] * s;
-			y = this.y + dr[d] * s;
+
+			this.x = (this.x + dr[d] * this.s) % N;
+			this.y = (this.y + dc[d] * this.s) % N;
+			//만약에 -7 N=5인경우
+			if(this.x<0)
+			{
+				this.x+=(((this.x*-1)/N)+1)*N;
+			}
+			if(this.y<0)
+			{
+				this.y+=(((this.y*-1)/N)+1)*N;
+			}
+
+
+		}
+
+		@Override
+		public String toString() {
+			return "ball [x=" + x + ", y=" + y + ", m=" + m + ", s=" + s + ", d=" + d + "]";
 		}
 
 	}
@@ -40,10 +57,10 @@ public class boj_20056_마법사상어와파이어볼 {
 		M = Integer.parseInt(st.nextToken());
 		K = Integer.parseInt(st.nextToken());
 		ArrayList<ball> list = new ArrayList<>();
-		ArrayList<ball> temp = new ArrayList<>();
+		ArrayList<ball> copylist = new ArrayList<>();
+		int r, c, m, s, d;
 		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
-			int r, c, m, s, d;
 			r = Integer.parseInt(st.nextToken()) - 1;
 			c = Integer.parseInt(st.nextToken()) - 1;
 			m = Integer.parseInt(st.nextToken());
@@ -51,23 +68,92 @@ public class boj_20056_마법사상어와파이어볼 {
 			d = Integer.parseInt(st.nextToken());
 			list.add(new ball(r, c, m, s, d));
 		}
+		int ac = 0;
+		int bc = 0;
+		int cx = 0;
+		int cy = 0;
+		int cd = -1;
+		int msum = 0;
+		int ssum = 0;
+		int size = 0;
 		while (K > 0) {
-
-			for (int i = 0; i < list.size(); i++)
+			copylist.clear();
+			for (int i = 0; i < list.size(); i++) {
 				list.get(i).move();
+
+			}
+
 			Collections.sort(list, new Comparator<ball>() {
 
 				@Override
 				public int compare(ball o1, ball o2) {
-					// TODO Auto-generated method stub
+					if (o1.x == o2.x)
+						return o1.y - o2.y;
 					return o1.x - o2.x;
 				}
 			});
-			
-			//이동 후 정렬이 다 되어있음
+
+			for (int i = 0; i < list.size() - 1; i++) {
+				if (list.get(i).x == list.get(i + 1).x && list.get(i).y == list.get(i + 1).y) {
+					// 다음 애랑 같네?! 2번 조건 만족!
+					size = 0;
+					ac = 0;
+					bc = 0; // 홀수 짝수 갯수를 세기 위해 각각 0으로 다시 초기화
+					msum = 0; // 질량의 합을 0으로 초기화
+					ssum = 0; // ...
+					cx = list.get(i).x;
+					cy = list.get(i).y; // 기준점을 잡아둔다! 특히나 2번조건을 만족해서 현재 상태는 삭제하기 때문에 미리 저장해둠
+					for (int j = i; j < list.size(); j++) {
+						ball ball = list.get(j);
+						if (ball.x == cx && ball.y == cy) {
+							// 좌표가 같으니까 넣을거야
+
+							msum += ball.m; // 질량 계산해주고~
+							ssum += ball.s;
+							if (ball.d % 2 == 0)
+								bc++;
+							else
+								ac++; // 짝 홀 갯수 세줄거
+							size++; // ⌊(합쳐진 파이어볼 속력의 합)/(합쳐진 파이어볼의 개수) size도 세자 ㅎㅎ
+							list.remove(j--);
+
+						} else
+							break;
+					}
+					i--;
+
+					// 자 ! 2번조건에 필요한 정보 ac,bc size를 다 구했다 깔깔
+					m = msum / 5;
+					if (m == 0)
+						continue; // 2.4번조건
+					s = ssum / size;
+					// 기준 방향을 잡을것이다
+					if ((ac > 0 && bc == 0) || (ac == 0 && bc > 0)) {
+						// 모두 홀수이거나 모두 짝수이면
+						cd = 0; // 기준방향 0
+					} else {
+						cd = 1;
+					}
+
+					for (int idx = 0; idx < 4; idx++) {
+						copylist.add(new ball(cx, cy, m, s, cd + (idx * 2)));
+					}
+
+				}
+
+			}
+			list.addAll(copylist);
+			// 이동 후 정렬이 다 되어있음
 
 			K--;
 		}
+		// K번 연산완료
+		long sum = 0l;
+		for (int i = 0; i < list.size(); i++)
+			sum += list.get(i).m;
+
+		System.out.println(sum);
+
 	}
 
 }
