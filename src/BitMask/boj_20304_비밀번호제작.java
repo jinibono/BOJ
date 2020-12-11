3eba2498698a5b5c04efa3efcf009a6bfed0936e
jinibono <jinibonoo@hanmail.net>
2020-12-11 23:43:58 +0900

    2020-12-11

--- src/BitMask/boj_20304_비밀번호제작.java
+++ src/BitMask/boj_20304_비밀번호제작.java
@@ -1,13 +1,13 @@
 package BitMask;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.*;
 
 public class boj_20304_비밀번호제작 {
 
 	public static void main(String[] args) {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+		
 	}
-	//commiter change2
+	// commiter change2
 }
